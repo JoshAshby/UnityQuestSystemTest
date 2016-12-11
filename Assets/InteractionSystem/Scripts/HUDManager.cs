42d37a8d482b39f03678e4c53aef06f8e81ec97b
JoshAshby <joshuaashby@joshashby.com>
2016-12-11 00:23:20 -0700

    trying some look animations

--- Assets/InteractionSystem/Scripts/HUDManager.cs
+++ Assets/InteractionSystem/Scripts/HUDManager.cs
@@ -7,48 +7,71 @@ public class HUDManager : MonoBehaviour
     public CanvasGroup Reticle = null;
     public CanvasGroup ReticleInfo = null;
 
+    private CanvasGroup _canvasGroup = null;
+
     private CrosshairSystem _crosshair = null;
-    private string _info = "";
+
+    private Animator _reticleAnimator = null;
+    private Text _infoText = null;
+
+    [Inject]
+    private IGameManager _gameManager;
 
     private void Awake()
     {
         _crosshair = GetComponentInParent<CrosshairSystem>();
+        _infoText = ReticleInfo.GetComponentInChildren<Text>();
+        _reticleAnimator = Reticle.GetComponent<Animator>();
+        _canvasGroup = GetComponent<CanvasGroup>();
 
         ReticleInfo.alpha = 0f;
 
         _crosshair.OnLookChange += OnLookChange;
+        _crosshair.OnInteract += OnInteract;
     }
 
     private void Destroy() {
         _crosshair.OnLookChange -= OnLookChange;
+        _crosshair.OnInteract -= OnInteract;
     }
 
     private void Update()
     {
-        ReticleInfo.GetComponentInChildren<Text>().text = _info;
+        if(_gameManager.fsm.CurrentState == GameStates.Menu) {
+            _canvasGroup.alpha = 0f;
+            return;
+        }
 
-        if (_info != "")
-            ReticleInfo.alpha = 1f;
-        else
-            ReticleInfo.alpha = 0f;
+        _canvasGroup.alpha = 1f;
     }
 
     public void OnLookChange(Transform obj)
     {
         Debug.Log("Encountered object");
         if (obj != null)
-            ShowInfo(obj.GetComponentInParent<InteractiveSettings>().Name);
+           {
+            _reticleAnimator.SetTrigger("Look");
+            ShowInfo(obj.GetComponent<InteractiveSettings>().Name);
+        }
         else
+        {
             ClearInfo();
+        }
+    }
+
+    public void OnInteract(Transform obj) {
+        _reticleAnimator.SetTrigger("Interact");
     }
 
     public void ClearInfo()
     {
-        _info = "";
+        ReticleInfo.alpha = 0f;
+        _infoText.text = "";
     }
 
     public void ShowInfo(string info)
     {
-        _info = info;
+        _infoText.text = info;
+        ReticleInfo.alpha = 1f;
     }
 }
