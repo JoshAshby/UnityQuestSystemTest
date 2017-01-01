df12d19bd64e5348dccd534f52bbda0a75bf2f87
JoshAshby <joshuaashby@joshashby.com>
2016-12-31 22:52:28 -0700

    remove things

--- Assets/Scripts/ICubicHermiteSpline.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface ICubicHermiteSpline
-{
-    Vector3 GetPoint(float t, int k);
-    Vector3 GetFirstDerivative(float t, int k);
-    Vector3 GetSecondDerivative(float t, int k);
-}
