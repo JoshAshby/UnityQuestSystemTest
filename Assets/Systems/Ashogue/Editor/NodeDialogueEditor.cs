4bdf802bb0a52bb50979e8c35862291a453efcef
JoshAshby <joshuaashby@joshashby.com>
2017-01-26 22:18:32 -0700

    basic node editor for dialogue \o/

--- Assets/Systems/Ashogue/Editor/NodeDialogueEditor.cs
+++ Assets/Systems/Ashogue/Editor/NodeDialogueEditor.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using Ashode;
 using Ashogue.Data;
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 class CallbackEvent<T>
 {
@@ -48,93 +50,167 @@ static class ContextMenuHanders
     }
 }
 
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+class NodeBelongsToAttribute : Attribute
+{
+    public Type CanvasType { get; set; }
+
+    public NodeBelongsToAttribute(Type canvasType)
+    {
+        this.CanvasType = canvasType;
+    }
+}
+
 class DialogueCanvas : NodeCanvas
 {
     public DialogueCanvas() : base() { }
 }
 
+[NodeBelongsTo(typeof(DialogueCanvas))]
 class StartNodeCanvasNode : Node
 {
-    private Vector2 _minSize = new Vector2(200, 100);
-    public override Vector2 MinSize { get { return _minSize; } set { _minSize = value; } }
+    public override Vector2 MinSize { get { return new Vector2(100, 40); } }
+    public override bool CanResize { get { return false; } }
 
-    private string _title = "Text Node";
-    public override string Title { get { return _title; } set { _title = value; } }
+    public override string Title { get { return "Start Node"; } }
 
     public StartNodeCanvasNode(INodeCanvas parent, Vector2 pos) : base(parent, pos) { }
 
     public override void SetupKnobs()
     {
-        AddKnob("start", NodeSide.Right, 1, Direction.Output, typeof(string));
+        AddKnob("start", NodeSide.Right, 1, Direction.Output, typeof(string)).Removable = false;
     }
 
     public override void OnGUI()
     {
-
+        DrawKnob("start", Rect.size.y / 2);
     }
 }
 
+[NodeBelongsTo(typeof(DialogueCanvas))]
 class TextNodeCanvasNode : Node
 {
-    private Vector2 _minSize = new Vector2(200, 100);
-    public override Vector2 MinSize { get { return _minSize; } set { _minSize = value; } }
+    public override Vector2 MinSize { get { return new Vector2(200, 300); } }
+    public override bool CanResize { get { return true; } }
 
-    private string _title = "Text Node";
-    public override string Title { get { return _title; } set { _title = value; } }
+    public override string Title { get { return "Text Node"; } }
 
     public TextNodeCanvasNode(INodeCanvas parent, Vector2 pos) : base(parent, pos) { }
 
+    public override void SetupKnobs()
+    {
+        AddKnob("in", NodeSide.Left, 0, Direction.Input, typeof(string)).Removable = false;
+    }
+
+    Dictionary<string, string> knobDisplayText = new Dictionary<string, string>();
+    string Text = "";
+
+    private string removeKnob = null;
     public override void OnGUI()
     {
+        DrawKnob("in", 20);
 
+        GUILayout.BeginVertical();
+        Text = GUILayout.TextArea(Text, GUILayout.Height(100));
+
+        if (GUILayout.Button("Add Branch"))
+        {
+            IKnob knob = AddKnob(Guid.NewGuid().ToString(), NodeSide.Right, 0, Direction.Output, typeof(string));
+            knobDisplayText.Add(knob.ID, "");
+        }
+
+        foreach (var knob in Knobs.Where(x => x.Value.Direction == Direction.Output))
+        {
+            GUILayout.BeginHorizontal();
+            knobDisplayText[knob.Key] = GUILayout.TextField(knobDisplayText[knob.Key]);
+            DrawKnob(knob.Key);
+            if (GUILayout.Button("X", GUILayout.ExpandWidth(false)))
+                removeKnob = knob.Key;
+            GUILayout.EndHorizontal();
+        }
+
+        if (!string.IsNullOrEmpty(removeKnob))
+        {
+            RemoveKnob(removeKnob);
+            knobDisplayText.Remove(removeKnob);
+            removeKnob = null;
+        }
+
+        GUILayout.EndVertical();
     }
 }
 
+[NodeBelongsTo(typeof(DialogueCanvas))]
 class EventNodeCanvasNode : Node
 {
-    private Vector2 _minSize = new Vector2(200, 100);
-    public override Vector2 MinSize { get { return _minSize; } set { _minSize = value; } }
+    public override Vector2 MinSize { get { return new Vector2(100, 40); } }
+    public override bool CanResize { get { return true; } }
 
-    private string _title = "Event Node";
-    public override string Title { get { return _title; } set { _title = value; } }
+    public override string Title { get { return "Event Node"; } }
 
     public EventNodeCanvasNode(INodeCanvas parent, Vector2 pos) : base(parent, pos) { }
 
+    public override void SetupKnobs()
+    {
+        AddKnob("in", NodeSide.Left, 0, Direction.Input, typeof(string)).Removable = false;
+        AddKnob("out", NodeSide.Right, 1, Direction.Output, typeof(string)).Removable = false;
+    }
+
+    string Msg = "";
     public override void OnGUI()
     {
 
+        DrawKnob("in", 20);
+        DrawKnob("out", 20);
+
+        Msg = GUILayout.TextField(Msg);
     }
 }
 
+[NodeBelongsTo(typeof(DialogueCanvas))]
 class WaitNodeCanvasNode : Node
 {
-    private Vector2 _minSize = new Vector2(200, 100);
-    public override Vector2 MinSize { get { return _minSize; } set { _minSize = value; } }
+    public override Vector2 MinSize { get { return new Vector2(100, 40); } }
+    public override bool CanResize { get { return true; } }
 
-    private string _title = "Wait Node";
-    public override string Title { get { return _title; } set { _title = value; } }
+    public override string Title { get { return "Wait Node"; } }
 
     public WaitNodeCanvasNode(INodeCanvas parent, Vector2 pos) : base(parent, pos) { }
 
+    public override void SetupKnobs()
+    {
+        AddKnob("in", NodeSide.Left, 0, Direction.Input, typeof(string)).Removable = false;
+        AddKnob("out", NodeSide.Right, 1, Direction.Output, typeof(string)).Removable = false;
+    }
+
+    int Wait = 0;
     public override void OnGUI()
     {
+        DrawKnob("in", 20);
+        DrawKnob("out", 20);
 
+        Wait = EditorGUILayout.IntField(Wait);
     }
 }
 
+[NodeBelongsTo(typeof(DialogueCanvas))]
 class EndNodeCanvasNode : Node
 {
-    private Vector2 _minSize = new Vector2(200, 100);
-    public override Vector2 MinSize { get { return _minSize; } set { _minSize = value; } }
+    public override Vector2 MinSize { get { return new Vector2(100, 40); } }
+    public override bool CanResize { get { return false; } }
 
-    private string _title = "End Node";
-    public override string Title { get { return _title; } set { _title = value; } }
+    public override string Title { get { return "End Node"; } }
 
     public EndNodeCanvasNode(INodeCanvas parent, Vector2 pos) : base(parent, pos) { }
 
+    public override void SetupKnobs()
+    {
+        AddKnob("in", NodeSide.Left, 0, Direction.Input, typeof(string)).Removable = false;
+    }
+
     public override void OnGUI()
     {
-
+        DrawKnob("in", 20);
     }
 }
 
@@ -162,7 +238,7 @@ public class NodeDialogueEditor : EditorWindow
 
         Canvas.InputSystem.AddHandlersFrom(typeof(ContextMenuHanders));
 
-        Canvas.State.AddNode(typeof(StartNodeCanvasNode), new Vector2(0, 0));
+        Canvas.State.AddNode(typeof(StartNodeCanvasNode), new Vector2(10, 10));
     }
 
     private void OnDestroy()
