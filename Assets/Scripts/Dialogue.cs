dbf8441861ef9ebde8f1b804c615152fcdf61332
JoshAshby <joshuaashby@joshashby.com>
2017-02-14 22:27:22 -0700

    store any type of data in the query
    specific criteria classes for different matching patterns
    not good for speed, supposedly, but good for readability and usability?

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,48 +1,66 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 namespace Dialogue
 {
+    public interface IGenericData
+    {
+        Type Type { get; }
+        object Value { get; }
+
+        IGenericData<TResult> OfType<TResult>();
+    }
+
+    public interface IGenericData<T>
+    {
+        T Value { get; set; }
+    }
+
+    public class GenericData<T> : IGenericData, IGenericData<T>
+    {
+        public GenericData(T val)
+        {
+            Value = val;
+        }
+
+        public Type Type { get { return typeof(T); } }
+        private T _value = default(T);
+        public object Value
+        {
+            get { return (object)_value; }
+            set { _value = (T)value; }
+        }
+
+        object IGenericData.Value
+        {
+            get { return (object)Value; }
+        }
+
+        T IGenericData<T>.Value
+        {
+            get { return (T)Value; }
+            set { Value = (object)value; }
+        }
+
+        public IGenericData<TResult> OfType<TResult>()
+        {
+            if (this is IGenericData<TResult>)
+                return (IGenericData<TResult>)this;
+            else
+                return null;
+        }
+    }
+
     class Query
     {
-        public Dictionary<string, int> Context;
+        public Dictionary<string, IGenericData> Context;
 
         public Query()
         {
-            Context = new Dictionary<string, int>();
+            Context = new Dictionary<string, IGenericData>();
         }
-    }
-
-    interface IQueryBuilderAdd
-    {
-        IQueryBuilderAdd Add(string key, string val);
-        IQueryBuilderAdd Add(string key, int val);
-    }
-
-    class QueryBuilder : IQueryBuilderAdd
-    {
-        private Query _query;
-
-        public IQueryBuilderAdd New()
-        {
-            _query = new Query();
-            return this;
-        }
-
-        public IQueryBuilderAdd Add(string key, string val)
-        {
-            _query.Context.Add(key, val.GetHashCode());
-            return this;
-        }
-
-        public IQueryBuilderAdd Add(string key, int val)
-        {
-            _query.Context.Add(key, val);
-            return this;
-        }
-
-        public Query Query { get { return _query; } }
     }
 
     class DatabasePartition
@@ -67,12 +85,12 @@ namespace Dialogue
 
     class Entry
     {
-        public List<Criteron> Criteria;
+        public List<ICriteron> Criteria;
         public object Payload;
 
         public Entry()
         {
-            Criteria = new List<Criteron>();
+            Criteria = new List<ICriteron>();
         }
 
         public int Length
@@ -82,50 +100,100 @@ namespace Dialogue
 
         public bool Check(Query query)
         {
-            return Criteria.All(x => x.Check(query.Context));
+            return Criteria.All(x => x.Check(query));
         }
     }
 
-    class Criteron
+    interface ICriteron
     {
-        protected int AccessKey;
+        string AccessKey { get; }
 
-        protected int UpperValue;
-        protected int LowerValue;
+        bool Check(Query query);
+    }
 
-        public Criteron(string key, string value)
+    class MatchCriteron<T> : ICriteron
+    {
+        public string AccessKey { get; set; }
+        private T _compareValue;
+
+        public MatchCriteron(string key, T val)
         {
-            AccessKey = key.GetHashCode();
+            AccessKey = key;
 
-            UpperValue = value.GetHashCode();
-            LowerValue = value.GetHashCode();
+            _compareValue = val;
         }
 
-        public Criteron(string key, int value)
+        public bool Check(Query query)
         {
-            AccessKey = key.GetHashCode();
-
-            UpperValue = value;
-            LowerValue = value;
-        }
-
-        public Criteron(string key, int lower, int upper)
-        {
-            AccessKey = key.GetHashCode();
-
-            UpperValue = upper;
-            LowerValue = lower;
-        }
-
-        public bool Check(Dictionary<int, int> state)
-        {
-            if (!state.ContainsKey(AccessKey))
+            if (!query.Context.ContainsKey(AccessKey))
                 return false;
 
-            int value = state[AccessKey];
+            IGenericData<T> val = query.Context[AccessKey].OfType<T>();
 
-            return value >= LowerValue && UpperValue >= value;
+            if (val == null)
+                return false;
+
+            return val.Value.Equals(_compareValue);
         }
+    }
+
+    class IntRangeCriteron : ICriteron
+    {
+        public string AccessKey { get; set; }
+        private int _lowCompareValue;
+        private int _highCompareValue;
+
+        public IntRangeCriteron(string key, int low, int high)
+        {
+            AccessKey = key;
+
+            _lowCompareValue = low;
+            _highCompareValue = high;
+        }
+
+        public bool Check(Query query)
+        {
+            if (!query.Context.ContainsKey(AccessKey))
+                return false;
+
+            IGenericData<int> val = query.Context[AccessKey].OfType<int>();
+
+            if (val == null)
+                return false;
+
+            return _lowCompareValue <= val.Value && val.Value <= _highCompareValue;
+        }
+    }
+
+    interface IQueryBuilderAdd
+    {
+        IQueryBuilderAdd Add(string key, string val);
+        IQueryBuilderAdd Add(string key, int val);
+    }
+
+    class QueryBuilder : IQueryBuilderAdd
+    {
+        private Query _query;
+
+        public IQueryBuilderAdd New()
+        {
+            _query = new Query();
+            return this;
+        }
+
+        public IQueryBuilderAdd Add(string key, string val)
+        {
+            _query.Context.Add(key, new GenericData<string>(val));
+            return this;
+        }
+
+        public IQueryBuilderAdd Add(string key, int val)
+        {
+            _query.Context.Add(key, new GenericData<int>(val));
+            return this;
+        }
+
+        public Query Query { get { return _query; } }
     }
 
     interface IEntryBuilderCriteria
@@ -153,19 +221,19 @@ namespace Dialogue
 
         public IEntryBuilderCriteria AddCriteron(string key, string val)
         {
-            _entry.Criteria.Add(new Criteron(key, val));
+            _entry.Criteria.Add(new MatchCriteron<string>(key, val));
             return this;
         }
 
         public IEntryBuilderCriteria AddCriteron(string key, int val)
         {
-            _entry.Criteria.Add(new Criteron(key, val));
+            _entry.Criteria.Add(new MatchCriteron<int>(key, val));
             return this;
         }
 
         public IEntryBuilderCriteria AddCriteron(string key, int low, int high)
         {
-            _entry.Criteria.Add(new Criteron(key, low, high));
+            _entry.Criteria.Add(new IntRangeCriteron(key, low, high));
             return this;
         }
     }
@@ -174,7 +242,7 @@ namespace Dialogue
     {
         IDatabaseBuilderEntry New();
     }
-    
+
     class DatabaseBuilder : IDatabaseBuilderEntry
     {
         private DatabasePartition _database;
@@ -185,7 +253,7 @@ namespace Dialogue
             return this;
         }
 
-        public EntryBuilder AddEntry(object payload)
+        public IEntryBuilderCriteria AddEntry(object payload)
         {
             EntryBuilder entryBuilder = new EntryBuilder();
             entryBuilder.New().SetPayload(payload);
@@ -223,34 +291,34 @@ namespace Dialogue
             Debug.Assert(testPartition.Query(queryBuilder.Query) == null);
 
             queryBuilder.New().Add("test1", "alpha");
-            Debug.Assert((string) testPartition.Query(queryBuilder.Query) == "wat");
+            Debug.Assert((string)testPartition.Query(queryBuilder.Query) == "wat");
 
             queryBuilder.New().Add("test2", 5);
-            Debug.Assert((string) testPartition.Query(queryBuilder.Query) == null);
+            Debug.Assert((string)testPartition.Query(queryBuilder.Query) == null);
 
             queryBuilder.New().Add("test2", 6);
-            Debug.Assert((string) testPartition.Query(queryBuilder.Query) == "bat");
+            Debug.Assert((string)testPartition.Query(queryBuilder.Query) == "bat");
 
             queryBuilder.New().Add("test3", 5);
-            Debug.Assert((string) testPartition.Query(queryBuilder.Query) == null);
+            Debug.Assert((string)testPartition.Query(queryBuilder.Query) == null);
 
             queryBuilder.New().Add("test3", 13);
-            Debug.Assert((string) testPartition.Query(queryBuilder.Query) == null);
+            Debug.Assert((string)testPartition.Query(queryBuilder.Query) == null);
 
             queryBuilder.New().Add("test3", 10);
-            Debug.Assert((string) testPartition.Query(queryBuilder.Query) == "mat");
+            Debug.Assert((string)testPartition.Query(queryBuilder.Query) == "mat");
 
             queryBuilder.New()
                 .Add("test4", "robin")
                 .Add("test5", 19)
                 .Add("test6", 7);
-            Debug.Assert((string) testPartition.Query(queryBuilder.Query) == null);
+            Debug.Assert((string)testPartition.Query(queryBuilder.Query) == null);
 
             queryBuilder.New()
                 .Add("test4", "robin")
                 .Add("test5", 19)
                 .Add("test6", 3);
-            Debug.Assert((string) testPartition.Query(queryBuilder.Query) == "hat");
+            Debug.Assert((string)testPartition.Query(queryBuilder.Query) == "hat");
         }
     }
 }
